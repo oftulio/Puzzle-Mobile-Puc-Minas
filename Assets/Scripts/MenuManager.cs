482ebd8600faff79adf8d66bef03779d1730f861
Leowr404 <Leonardounity404@gmail.com>
2025-02-28 05:12:43 -0300

    Menu Animations Test

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject painelSettings;
+    public RectTransform[] botoes;
+    public RectTransform rawImage;
+    public float moveDistance; // Distância que o botão vai oscilar
+    public float moveSpeedRaw;
+    public float duration; // Tempo da animação
     void Start()
     {
         painelSettings.SetActive(false);
+        rawImage.DOLocalMoveX(rawImage.localPosition.x + moveSpeedRaw, duration).SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutSine);
+        foreach (RectTransform botao in botoes)
+        {
+            botao.DOLocalMoveX(botao.localPosition.x + moveDistance, duration)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.InOutSine);
+        }
         
     }
 
