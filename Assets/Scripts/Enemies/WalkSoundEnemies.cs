01755e36873fa13eef1d387e86f7b8005f7fdb62
Leowr404 <Leonardounity404@gmail.com>
2025-06-29 20:04:50 -0300

    add walkscript sound

---
+++ Assets/Scripts/Enemies/WalkSoundEnemies.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WalkSoundEnemies : MonoBehaviour
+{
+    public AudioSource footstepAudio;
+    public AudioClip[] footstepClips; // Vários sons para variar
+
+    public void PlayFootstep()
+    {
+        if (footstepClips.Length > 0)
+        {
+            int index = Random.Range(0, footstepClips.Length);
+            footstepAudio.PlayOneShot(footstepClips[index]);
+        }
+        else if (!footstepAudio.isPlaying)
+        {
+            footstepAudio.Play();
+        }
+    }
+}
